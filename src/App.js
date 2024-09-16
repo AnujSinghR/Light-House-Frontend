3228ea2ea485c9d0b6a0ef651238177c3c2aa8d9
AnujSinghR <anujsingh0r@gmail.com>
2024-09-16 12:49:07 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import axios from 'axios';
 
 function App() {
     const [Report,setReport] = useState();
+    const [JobId,setJobId] = useState();
     const [showReport, setShowReport] = useState(false);
+    // Helper function to wait for a specified amount of time
+    const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     let getReport = async(Url) =>{
       console.log(Url)
      await axios.post('https://light-house-backend.vercel.app/lighthouse',{url:Url})
       .then(function (response) {
-        setReport(response);
+        setJobId(response);
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
+      console.log("wait for 90s");
+      //set wait method for wait for getting report then hit api again nerly 90 s
+      wait(90000);
+      await axios.post('https://light-house-backend.vercel.app/lighthouse',{JobId:JobId})
+        .then(function (response){
+          setReport(response);
+          console.log(Report);
+        })
+        .catch(function (error){
+          console.error(error);
+        })
       setShowReport(true);
     }
 
